5318270340840993bb607e0e2bde7e7e5929e8c9
LLschuster <perez99leo@outlook.es>
2019-05-12 11:39:02 +0200

    Setup of place and aiming mecanics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './components/Canvas';
 
+class App extends Component {
+  componentDidMount() {
+    const self = this;
+    setInterval(() => {
+      self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <p>Alien invasion Game</p>
-    </div>
-  );
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
+  render() {
+    return (
+      <Canvas
+        angle={this.props.angle}
+        trackMouse={event => (this.trackMouse(event))}
+      />
+    );
+  }
 }
+
+App.propTypes = {
+  angle: PropTypes.number.isRequired,
+  moveObjects: PropTypes.func.isRequired,
+};
 
 export default App;
